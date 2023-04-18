9849a127b99a4e70b38bab44c4cdc1251b28c48d
Your Name <you@example.com>
2023-04-18 17:06:01 +0100

    changes

---
+++ forum/data.go
@@ -0,0 +1,10 @@
+package forum
+
+import "net/http"
+
+func addPosts(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+
+	}
+}
--- main.go
+++ main.go
@@ -2,50 +2,25 @@ package main
 
 import (
 	"fmt"
-	"html/template"
+	"forumProject/forum"
 	"net/http"
 )
 
-type PostData struct {
-	PostTitle    string
-	PostContent  string
-	PostCategory string
-}
-
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/dashboard", dashboardHandler)
-	http.HandleFunc("/dashbaord", postHandler)
+	http.HandleFunc("/", forum.LoginHandler)
+	http.HandleFunc("/dashboard", forum.PostHandler)
+	http.HandleFunc("/post", forum.PostHandler)
 
 	fmt.Println("Server listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
 
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "forum/static/index.html")
-}
+// func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "forum/static/dashboard.html")
+// }
 
-func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "forum/static/dashboard.html")
-}
+// func postHandler(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "forum/static/dashboard.html")
 
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	data := PostData{
-		PostTitle:    "My Post Title",
-		PostContent:  "This is the content of my post.",
-		PostCategory: "Technology",
-	}
-
-	tmpl, err := template.ParseFiles("dashboard.html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
+// }
